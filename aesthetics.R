224fbad880f9cae44746a37e704ef013ccb3f22f
Colom <sara.colom@ucsf.edu>
2023-02-03 14:42:41 -0800

    create r file w ucsf colors & rmd file for data demos

---
+++ aesthetics.R
@@ -0,0 +1,21 @@
+# Custom theme
+
+# UCSF colors
+
+# Blues from primary color slide (top down in order)
+blue1 <- "#00264F"
+blue2 <- "#06408B"
+blue3 <- "#0668B3"
+blue4 <- "#0092D2"
+blue5 <- "#E4F5FD"
+greyblue <- "#C7CFDA"
+
+# Secondary colors
+
+sec_col1 <- "#00545E"
+sec_col2 <- "#008B9A"
+sec_col3 <- "#00B0BD"
+sec_col4 <- "#60D0DA"
+sec_col5 <- "#00B14C"
+sec_col6 <- "#84C234"
+
